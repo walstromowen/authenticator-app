f6ac3d78496495bbb7b584cb6f1211da75e3b56d
walstromowen <walstrom.owen@gmail.com>
2025-03-23 19:18:04 -0500

    Added build command

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import App from './App.jsx'
 import {disableReactDevTools} from '@fvilers/disable-react-devtools';
 
-if(process.env.NODE_ENV === 'production') disableReactDevTools
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
