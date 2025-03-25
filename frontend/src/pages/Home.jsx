5e0d2175d250705235c18edc329a7e058911a502
walstromowen <walstrom.owen@gmail.com>
2025-03-25 15:50:04 -0500

    Added animation to home page2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,8 +5,8 @@ export default function Home(){
         <div className='home-page-container'>
            <h1 className="home-header neon">
             U<span className='delay-1'>s</span>
-            er Aut<span className='delay-1'>h</span>
-            entica<span className='delay-1'>t</span>
+            er Aut<span className='delay-2'>h</span>
+            entica<span className='delay-3'>t</span>
             or
             </h1>
         </div>
