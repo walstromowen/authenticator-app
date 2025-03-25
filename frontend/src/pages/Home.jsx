64a9b2410a914b73e3684fe9094cc2cb1eebb362
walstromowen <walstrom.owen@gmail.com>
2025-03-25 15:43:24 -0500

    Added animation to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,12 @@
 export default function Home(){
     return(
         <div className='home-page-container'>
-           <h1 className="home-header">User Authenticator</h1>
+           <h1 className="home-header neon">
+            U<span className='delay-1'>s</span>
+            er Aut<span className='delay-1'>h</span>
+            entica<span className='delay-1'>t</span>
+            or
+            </h1>
         </div>
     )
 }
